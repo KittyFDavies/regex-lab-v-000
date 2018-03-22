d531b262f873038b148685641b3a5e207b70cb70
Kitty Davies <kittyfdavies@gmail.com>
2018-03-22 23:46:15 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,7 @@ end
 
 def words_five_letters_long(text)
   text = text.split(" ")
-  text.grep(/^\w{5}\s/)
+  text.grep(/^\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
