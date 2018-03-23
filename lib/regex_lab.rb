2aa683e7a7635b8a27c20eac789cfcc8e4906d59
Kitty Davies <kittyfdavies@gmail.com>
2018-03-23 00:46:47 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -27,7 +27,7 @@ end
 def valid_phone_number?(phone)
   phone = phone.split(" ")
   phone.each do |number|
-    if number.match(/^(\d{1}\D*){10}$/)
+    if number.match(/^(\D*\d{1}\D*){10}$/)
       true
     else
       false
